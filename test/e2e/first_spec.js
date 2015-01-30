a76dfa96c0c987455a771cf2a292612c001a18f4
Jose Balado <JoseBalado@users.noreply.github.com>
2015-01-30 21:11:21 +0100

    Protractor creates and deletes an appointment.

--- test/e2e/first_spec.js
+++ test/e2e/first_spec.js
@@ -1,3 +1,8 @@
+// 1. Checks if the application is working by asking the title. 
+// 2. Logs in with a previous registered user. 
+// 3. Creates a new appointment.
+// 4. Deletes it.
+
 var chai=require('chai');
 var chaiAsPromised=require('chai-as-promised');
 chai.use(chaiAsPromised);
@@ -12,7 +17,7 @@ describe('Agenda app:', function() {
   it('Is in the login page', function() {
     expect(element(by.css('#navbarCollapse > ul:nth-child(2)')).getText()).to.eventually.equal('Log in');
   });
-  it('Clicks the link "login page", inserts valid user and password and logs in.', function() {
+  it('Clicks the link "login page", inserts a valid user and password and logs in.', function() {
     element(by.css('#navbarCollapse > ul:nth-child(2)')).click();
     element(by.id('inputUsername')).sendKeys('Jose');
     element(by.id('inputPassword')).sendKeys('Jose');
@@ -34,8 +39,8 @@ describe('Adding an appointment:', function() {
   });
   it('Fills the fields for creating a new activity.', function() {
     element(by.id('nombre')).sendKeys('Protractor Appointment');
-    //element(by.id('fecha')).sendKeys('28-enero-2015');  Fix
-    //element(by.id('hora')).sendKeys('Jose');            Fix
+    //element(by.id('fecha')).sendKeys('28-enero-2015');  Fix or add
+    //element(by.id('hora')).sendKeys('Jose');            Fix or add
     element(by.id('lugar')).sendKeys('Meeting room');
     element(by.id('texto')).sendKeys('Hi there!');
   });
@@ -45,7 +50,25 @@ describe('Adding an appointment:', function() {
 });
 
 describe('Deletting the appointment just added:', function() {
-  it('Selects the appoinment just added.', function() {
+  it('Selects the appoinment.', function() {
+    browser.driver.manage().window().maximize(); // This tests do not work unless in full screen
+                                                 // because '.dropdown-toggle' is not accessible.
+    element.all(by.repeater('actividad in actividades')).filter(function(elem, index) {
+      return elem.getText().then(function(text) {
+        return text === 'Protractor Appointment';
+      });
+    }).then(function(filteredElements) {
+      (filteredElements[0]).click(); // It does not work if window < 790px
+    });
+  });
+  it('Clicks on the "Opciones" link.', function() {
+    element(by.css('.dropdown-toggle')).click();
+  });
+  it('Selects "Eliminar actividad".', function() {
+    element(by.css('[href="#myModal"]')).click();
+    browser.wait(function(){
+      return element(by.linkText("Eliminar")).isPresent();
+    })
+    element(by.linkText("Eliminar")).click();
   });
 });
-
